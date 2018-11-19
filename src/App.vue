82316fef7f3ebf978cc1fc3faf229c8503ee7319
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-18 23:38:12 -0600

    added - photo container, input panel, filter panel, data object, bootstrap imported

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-   
+    <h2>Filtrify Photo</h2>
+    <div class="photo--container">
+      <img 
+        class="photo--image" 
+        :src="imageSource" 
+        alt="">
+    </div>
+    <div class="image-loader container">
+      <div class="form-group">
+        <label for="custom-image-input">Custom Image URL</label>
+        <input type="text" class="form-control" id="custom-image-input" v-model="imageSource">
+      </div>
+    </div>
+    <div class="filter-panel">
+
+    </div>
   </div>
 </template>
 
@@ -10,7 +25,18 @@ export default {
   name: 'app',
   data () {
     return { 
-     
+      imageSource: '',
+      filters: {
+        blur: 0,          // px
+        brightness: 0,    // %
+        contrast: 0,      // %
+        grayscale: 0,     // %
+        hueRotate: 0,     // deg
+        invert: 0,        // %
+        opacity: 0,       // %
+        saturate: 0,      // %
+        sepia: 0,         // %
+      }
     }
   },
 
@@ -18,5 +44,26 @@ export default {
 </script>
 
 <style>
+@import './assets/css/bootstrapv3.css';
 
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.photo--container {
+  min-height: 80vh;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+}
+
+.photo--image {
+  max-height: 80vh;
+}
 </style>
