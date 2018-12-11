c17608cea66cb0f30b8e9d5018707fe1e5905361
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-10 21:34:23 -0600

    added - displayimage cmp for image uploaded display, passing image as prop

---
+++ src/components/image-container/DisplayImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="display--container my-4">
+    <img 
+      id="image"
+      class="display--image card" 
+      v-show="image.source"
+      :class="{ 'image-loaded': image.source }"
+      :src="image.source" 
+      alt="">
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['image']
+}
+</script>
+
+<style>
+
+</style>
